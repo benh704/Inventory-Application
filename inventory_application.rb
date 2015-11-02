101a644fbe5ed1fef686877e2e5aa035d45f75f1
benh704 <bhoff704@gmail.com>
2015-11-02 14:38:03 -0500

    Update inventory_application.rb

--- inventory_application.rb
+++ inventory_application.rb
@@ -1,11 +1,15 @@
   Inventory_item = {
--  attr_accessor :name		+          "Asparagus" => 3.29,
--  attr_accessor :price		+           "Carrots" => 2.99,
--  attr_accessor :quantity		+           "Squash" => 2.59
-+          }
-+ Inventory_item.each do |name, price|
-+   puts "#{name}: $#{price}"
-+ end
+         "Asparagus" => 3.29,
+          "Carrots" => 2.99,
+          "Squash" => 2.59
+          }
+ Inventory_item.each do |name, price|
+   puts "#{name}: $#{price}"
+ end
+# -  attr_accessor :name		+ 
+# -  attr_accessor :price		+ 
+# -  attr_accessor :quantity		+ 
+
 
 keylist = []
 price_hash = { }
@@ -35,5 +39,5 @@ puts "Here are the vegetable contents, sorted into order:"
 keylist = price_hash.keys.sort
 keylist.each do |key|
 puts "#{key} is #{price_hash[key]}."
-23 end
+ end
   
